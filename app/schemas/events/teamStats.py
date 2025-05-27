390955b350362636189895276c8f61031a24e7bf
Pedro Oliveira <peutroleo@gmail.com>
2025-05-27 19:46:36 -0300

    fix: response model to support dicts

--- app/schemas/events/teamStats.py
+++ app/schemas/events/teamStats.py
@@ -5,16 +5,16 @@ from pydantic import HttpUrl
 from app.schemas.base import HLTVBaseModel, AuditMixin
 
 class prizeDetails(HLTVBaseModel):
-    prize: int
-    club_share: int
+    prize: Optional[int]
+    club_share: Optional[int]
 
 class vrsDetails(HLTVBaseModel):
-    vrs_date: str
-    points_before_event: int
-    points_after_event: int
-    points_acquired: int
-    placement_before_event: int
-    placement_after_event: int
+    vrs_date: Optional[str]
+    points_before_event: Optional[int]
+    points_after_event: Optional[int]
+    points_acquired: Optional[int]
+    placement_before_event: Optional[int]
+    placement_after_event: Optional[int]
 
 class coachDetails(HLTVBaseModel):
     id: str
@@ -28,15 +28,17 @@ class lineupDetails(HLTVBaseModel):
 
 class eventTeamStatsDetails(HLTVBaseModel):
     team_placement: str
-    qualify_method: str
+    prize: Optional[List[prizeDetails]]
+    vrs: Optional[List[vrsDetails]]
+    qualify_method: Optional[str]
     lineup: List[lineupDetails]
     coach: Optional[List[coachDetails]]
-    vrs: Optional[List[vrsDetails]]
-    prize: Optional[List[prizeDetails]]
+    
+    
 
 
 
 class EventTeamStats(HLTVBaseModel, AuditMixin):
     event_id: str
     team_id: str
-    stats: List[eventTeamStatsDetails]
+    stats: eventTeamStatsDetails
