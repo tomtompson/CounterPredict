ad1b247f3d46682364cf50a0f5c4c6220123b5bc
Pedro Oliveira <peutroleo@gmail.com>
2026-05-08 14:41:43 -0300

    del: old test

--- b64.py
+++
@@ -1,5 +0,0 @@
-import base64
-token_real = "1418970711"
-token_ofuscado = base64.b64encode(token_real.encode()).decode()
-print(token_ofuscado)
-# Saída: NzE1MDU5MTU4MDpBQUhrZmdoSktMbU5PUFFSc3RVVldYeXoxMjM0NTY3ODkw
